c3432e2d2a4fe5ba6633ee00da63e44446abc2e6
Joel Capitao <jcapitao@redhat.com>
2024-09-26 16:03:19 +0200

    Add rdo-cs10 host

--- installer/bootstrap.sh
+++ installer/bootstrap.sh
@@ -5,15 +5,15 @@ sudo dnf update -y
 sudo dnf install -y git vim ansible-core libselinux-python3
 ansible-galaxy collection install ansible.posix community.general community.crypto
 
-if [ ! -f $HOME/.ssh/id_rsa ]; then
-  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
-fi
+#if [ ! -f $HOME/.ssh/id_rsa ]; then
+#  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+#fi
 
 rm -rf $HOME/ansible-role-dlrn
-git clone https://github.com/jcapiitao/ansible-role-dlrn.git $HOME/ansible-role-dlrn
+git clone https://github.com/jcapiitao/ansible-role-dlrn.git ./ansible-role-dlrn
 
 if [ ! -d $HOME/sf-infra ]; then
-  git clone https://github.com/softwarefactory-project/sf-infra $HOME/sf-infra
+  git clone https://github.com/softwarefactory-project/sf-infra ./sf-infra
 fi
 
 echo "Bootstrap OK"
